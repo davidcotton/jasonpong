71bba1065fb3ea4981a2832a1f974578cbe7ccae
Dave Cotton <david.f.cotton@gmail.com>
2019-05-05 14:19:24 +1000

    Move both actions into a single `step()` call

--- jasonpong/envs/pong.py
+++ jasonpong/envs/pong.py
@@ -23,60 +23,56 @@ class Pong(Env):
     def __init__(self):
         self.action_space = spaces.Discrete(1)
         self.observation_space = spaces.Discrete(5)
+        self.time = 0
         self.game_over = False
         self.winner = None
-        self.time = 0
-        self.player_turn = 0
-        self.paddle_position = None
+        self.paddle_positions = None
         self.ball_position = None
         self.ball_velocity = None
 
-    def step(self, action):
-        if self.game_over:
-            return
-
-        if action == Action.ACTION_LEFT.value:
-            self.paddle_position[self.player_turn] = max(self.paddle_position[self.player_turn] - 1, 0)
-        elif action == Action.ACTION_RIGHT.value:
-            self.paddle_position[self.player_turn] = min(self.paddle_position[self.player_turn] + 1, BOARD_WIDTH)
-
-        if self.player_turn == 1:
-            self._update()
-
-        state = self._get_state()[:]
-        if self.game_over:
-            reward = 1 if self.winner == self.player_turn else -1
-        else:
-            reward = 0
-        info = {}
-
-        self.player_turn = (self.player_turn + 1) % 2
-        if self.player_turn == 1:
-            self.time += 1
-
-        return state, reward, self.game_over, info
-
     def reset(self):
+        self.time = 0
         self.game_over = False
         self.winner = None
-        self.time = 0
-        self.player_turn = 0
-        self.paddle_position = [BOARD_WIDTH // 2, BOARD_WIDTH // 2]
+        self.paddle_positions = [BOARD_WIDTH // 2, BOARD_WIDTH // 2]
         self.ball_position = np.array([BOARD_WIDTH // 2, BOARD_HEIGHT // 2])
         self.ball_velocity = np.array([1, 1])
 
         return self._get_state()
+
+    def step(self, actions):
+        if self.game_over:
+            return
+
+        for player, action in enumerate(actions):
+            if action == Action.ACTION_LEFT.value:
+                self.paddle_positions[player] = max(self.paddle_positions[player] - 1, 0)
+            elif action == Action.ACTION_RIGHT.value:
+                self.paddle_positions[player] = min(self.paddle_positions[player] + 1, BOARD_WIDTH)
+
+        self._update()
+
+        state = self._get_state()[:]
+        if self.game_over:
+            reward = (1, -1) if self.winner == 0 else (-1, 1)
+        else:
+            reward = (0, 0)
+        info = {}
+
+        self.time += 1
+
+        return state, reward, self.game_over, info
 
     def _update(self):
         self.ball_position += self.ball_velocity
 
         # bounce off paddle
         if self.ball_position[1] == PADDLE_HEIGHT:
-            delta = abs(self.ball_position[0] - self.paddle_position[0])
+            delta = abs(self.ball_position[0] - self.paddle_positions[0])
             if delta <= (PADDLE_WIDTH // 2):
                 self.ball_velocity[1] *= -1
         elif self.ball_position[1] == (BOARD_HEIGHT - PADDLE_HEIGHT):
-            delta = abs(self.ball_position[0] - self.paddle_position[1])
+            delta = abs(self.ball_position[0] - self.paddle_positions[1])
             if delta <= (PADDLE_WIDTH // 2):
                 self.ball_velocity[1] *= -1
 
@@ -93,8 +89,7 @@ class Pong(Env):
             self.winner = 1
 
     def render(self, mode='human'):
-        print('Time:{} Player:{} Bat:{} Ball_P:({},{}) Ball_V:({},{})'.format(self.time, self.player_turn,
-                                                                              *self._get_state()))
+        print('Time:{} Paddles:({}, {}) Ball_P:({},{}) Ball_V:({},{})'.format(self.time, *self._get_state()))
 
     def _get_state(self):
-        return np.array([self.paddle_position[self.player_turn]] + list(self.ball_position) + list(self.ball_velocity))
+        return np.concatenate((np.array(self.paddle_positions), self.ball_position, self.ball_velocity), axis=0)
--- main.py
+++ main.py
@@ -1,25 +1,102 @@
-from jasonpong.envs.pong import Pong
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from jasonpong.envs.pong import Pong, BOARD_WIDTH, BOARD_HEIGHT, Action
+
+
+class Agent(ABC):
+
+    def __init__(self, player) -> None:
+        super().__init__()
+        self.player = player
+
+    @abstractmethod
+    def forward(self, state) -> int:
+        pass
+
+    def backwards(self, reward, game_over, next_state):
+        pass
+
+
+class SimpleAgent(Agent):
+    def forward(self, state) -> int:
+        if state[0] < 5:
+            action = 2  # right
+        elif state[0] > 5:
+            action = 1  # left
+        else:
+            action = 0
+        return action
+
+
+class QTableAgent(Agent):
+    def __init__(self, player) -> None:
+        super().__init__(player)
+        self.gamma = 0.95
+        self.learning_rate = 0.8
+        self.num_actions = len(Action)
+        self.q = np.zeros([BOARD_WIDTH, BOARD_WIDTH, BOARD_HEIGHT, self.num_actions])
+        self.epsilon = 0.1
+        self.last_state = None
+        self.last_action = None
+
+    def forward(self, state) -> int:
+        paddle_position = state[self.player] - 1
+        ball_x = state[2] - 1
+        ball_y = state[3] - 1
+        q = self.q[paddle_position, ball_x, ball_y]
+        if np.random.uniform() < self.epsilon:
+            action = np.random.randint(0, self.num_actions - 1)
+        else:
+            # action = np.argmax(q)
+            best_actions = np.argwhere(q == np.amax(q)).flatten()
+            action = np.random.choice(best_actions)
+        self.last_state = paddle_position, ball_x, ball_y
+        self.last_action = action
+        return action
+
+    def backwards(self, reward, game_over, next_state):
+        paddle_pos = self.last_state[0]
+        ball_x = self.last_state[1]
+        ball_y = self.last_state[2]
+        action = self.last_action
+
+        next_q = self.q[next_state[self.player]-1, next_state[2]-1, next_state[3]-1]
+        target_q = reward + self.gamma * np.max(next_q)
+        prev_q = self.q[paddle_pos, ball_x, ball_y, action]
+        self.q[paddle_pos, ball_x, ball_y, action] += self.learning_rate * (target_q - prev_q)
+        foo = 1
 
 
 def play_game():
     env = Pong()
-    initial_obs = env.reset()
-    obs = [initial_obs, initial_obs]
-    is_game_over = False
-    players = list(range(2))
-    while not is_game_over:
-        for player in players:
-            if obs[player][0] < 5:
-                action = 2  # right
-            elif obs[player][0] > 5:
-                action = 1  # left
-            else:
-                action = 0
-            next_obs, reward, is_game_over, _ = env.step(action)
-            obs[player] = next_obs
-            env.render()
-            if reward:
-                print('winner:', player)
+    agent_cls = QTableAgent
+    agents = [agent_cls(i) for i in range(2)]
+
+    for episode in range(10000):
+        # print('episode:', episode)
+        is_game_over = False
+        obs = env.reset()
+        step = 0
+
+        while not is_game_over:
+            actions = []
+            for agent in agents:
+                action = agent.forward(obs)
+                actions.append(action)
+
+            obs, rewards, is_game_over, _ = env.step(actions)
+
+            for player, agent in enumerate(agents):
+                reward = rewards[player]
+                agent.backwards(reward, is_game_over, obs)
+                # env.render()
+                if reward == 1:
+                    print('episode:', episode, 'winner:', player, 'steps', step)
+            step += 1
+        # print()
+    foo = 1
 
 
 if __name__ == '__main__':
