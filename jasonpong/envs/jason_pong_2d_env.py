d30e4c4513ed023b72546bd9c85237a81bdd602a
dave <david.f.cotton@gmail.com>
2019-05-16 20:43:28 +1000

    Add both players observation to reset()

--- jasonpong/envs/jason_pong_2d_env.py
+++ jasonpong/envs/jason_pong_2d_env.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 from gym import spaces
 
@@ -9,17 +11,25 @@ class JasonPong2dEnv(JasonPongEnv):
     def __init__(self):
         super().__init__()
         self.observation_space = spaces.Box(low=0, high=3, shape=(BOARD_HEIGHT, BOARD_WIDTH, 1), dtype=np.uint8)
-        self.obs_type = 'image'
+        # self.obs_type = 'image'
+
+    def reset(self) -> Tuple[np.ndarray, np.ndarray]:
+        player0_obs = super().reset()
+        self.player = 1
+        player1_obs = super().reset()
+        self.player = 0
+        return player0_obs, player1_obs
 
     def render(self, mode='human'):
         print(self._get_state())
 
     def _get_state(self) -> np.ndarray:
         state = np.zeros((BOARD_HEIGHT, BOARD_WIDTH), dtype=np.uint8)
-        state[self.ball_position[1], self.ball_position[0]] = 2
-        my_paddle_x = self.paddle_positions[self.player]
+        ball_x, ball_y = self.ball_position.astype(np.int8)
+        state[ball_y, ball_x] = 2
+        my_paddle_x = int(self.paddle_positions[self.player])
         my_paddle_y = 0 if self.player == 0 else BOARD_HEIGHT - 1
-        opp_paddle_x = self.paddle_positions[1 - self.player]
+        opp_paddle_x = int(self.paddle_positions[1 - self.player])
         opp_paddle_y = BOARD_HEIGHT - 1 if self.player == 0 else 0
         state[my_paddle_y, my_paddle_x] = 1
         state[opp_paddle_y, opp_paddle_x] = 3
